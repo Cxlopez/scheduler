2648fc46a5a2c2bce468b2b7452c336330ff8ac5
Cxlopez <cristian4402@hotmail.com>
2022-07-25 15:38:37 -0400

    create empy, header compononents & stories

--- src/components/Application.js
+++ src/components/Application.js
@@ -39,7 +39,7 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={day}
+            value={day}
             onChange={setDay}
           />
         </nav>
---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from "react";
+import "components/Appointment/styles.scss";
+
+
+export default function Appointment() {
+  
+
+  return (
+    <article className="appointment"></article>
+  );
+};
